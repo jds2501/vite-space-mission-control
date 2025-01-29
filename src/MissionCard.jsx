d39f72a231de6fb2a83bbce91aacef8ead7632df
Jason Smith <jasondsmith2501@gmail.com>
2025-01-29 13:36:09 -0800

    Some CSS cleanup

--- src/MissionCard.jsx
+++ src/MissionCard.jsx
@@ -2,7 +2,7 @@ function MissionCard({ id, name, status, crew }) {
     return (
         <div>
             <h3>{name}</h3>
-            <p>Status: <span className="status" id={id}>{status}</span></p>
+            <p>Status: <span>{status}</span></p>
             <p>Crew: {crew.join(", ")}</p>
         </div>
     );
